48b791f0cb954ceba8a1940574159f31e7b1040d
parisnic <nicolas.paris3@univ-grenoble-alpes.fr>
2023-10-17 14:20:23 +0200

    t

--- inversion/.ipynb_checkpoints/1_prepare_inversion_data-checkpoint.py
+++ inversion/.ipynb_checkpoints/1_prepare_inversion_data-checkpoint.py
@@ -17,6 +17,9 @@ lat_min, lat_max, lon_min, lon_max =
 
 day = '23AUG21' #denseigner dans le format YYMMMDD
 
+days_fit = ['23AUG21','23AUG21']
+
+
 ######################################## on load les fichiers de source et on les stack 
 
 stations = []
@@ -26,8 +29,29 @@ for filename in os.listdir(data_directory):
         df = pd.read_parquet(f'{data_directory}{filename}', sep=' ')
         idx_day = np.where(df['YYMMMDD'].values==day)[0]
         try : #in case there is no data for the given day at this station 
-            if lat_min<=df['_latitude(deg)'].values[idx_day]<= lat_max and lon_min<=df['_longitude(deg)'].values[idx_day]<= lon_max: #if  inside the window in which we put sources
+            lat_sta = df['_latitude(deg)'].values[idx_day]
+            lon_sta = df['_longitude(deg)'].values[idx_day]
+            if lat_min<=lat_sta<= lat_max and lon_min<=lon_sta<= lon_max: #if  inside the window in which we put sources
+                x = df['e(m)'].values[idx_day]
+                y = df['n(m)'].values[idx_day]
+                z = df['u(m)'].values[idx_day]
                 
+                #doit use les valeur de déplaxemene tavant hilary pour estimerd&placement jour j 
+                beg = np.where(df['YYMMMDD'].values==days_fit[0])[0]
+                end = np.where(df['YYMMMDD'].values==days_fit[1])[0]
+                
+                days_fit = np.arange(0,end-beg)
+                day_converted = idx_day-beg 
+                
+                x_fit = np.polyfit(days_fit,df['e(m)'].values[beg:end])
+                y_fit = np.polyfit(days_fit,df['n(m)'].values[beg:end])
+                z_fit = np.polyfit(days_fit,df['z(m)'].values[beg:end])
+                
+                x_val = x-np.polyval(x_fit, day_converted)
+                y_val = y-np.polyval(y_fit, day_converted)
+                z_val = z-np.polyval(z_fit, day_converted)
+                
+                stations.append([lat_sta, lon_sta, x_val, y_val, z_val])
                 
         except:
             pass
