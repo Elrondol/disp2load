33fda9a15d9d14843d0efcb80bc62812d718d643
Nicolas Paris nicolas.paris3@etu.univ-grenoble-alpes.fr <nicolas.paris3@etu.univ-grenoble-alpes.fr>
2023-10-18 10:23:24 +0200

    t

--- 1bis_merge_models.py
+++ 1bis_merge_models.py
@@ -4,7 +4,7 @@ import os
 """Ce script sert à merge les fichiers de déplacement associés à chacune des lignes de source """
 
 #################### OU VA T ON EXECUTER LE RUN #### -> les fichiers avec les lignes de sources seront rassemblés dans ce dossier 
-directory = 'run_test' 
+directory = 'run_test3' 
 
 
 ######################################## on load les fichiers de source et on les stack 
