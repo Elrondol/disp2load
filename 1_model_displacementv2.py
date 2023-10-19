e0d754339877f20883ff9a6ef412fabe60e02e5e
Nicolas Paris nicolas.paris3@etu.univ-grenoble-alpes.fr <nicolas.paris3@etu.univ-grenoble-alpes.fr>
2023-10-19 07:26:06 +0200

    t

--- 1_model_displacementv2.py
+++ 1_model_displacementv2.py
@@ -8,12 +8,12 @@ import tempfile
 ce script a une parallélisaiton naturelle : il créé un fiichier pour indiquer qu'il travail sur une ligne donnée  et il créé le fichier de la ligne et supprime le fichier lock -> on peut lancer le job autant de fois que l'on veut et chacun se mettra à travailler sur une ligne diféfrente  """
 
 #################### OU VA T ON EXECUTER LE RUN #### -> les fichiers avec les lignes de sources seront rassemblés dans ce dossier 
-directory = 'run_verif_convert2_corr' 
+directory = 'run_verif' 
 
 ######################################## SOURCES ET CONSTANTES ################
 
-ps_file_path = 'ps_verif_convert2.npy' #le chemin d'accès au fichier qui contient la grille de presisons  
-rs_file_path = 'rs_verif_convert2.npy' #le chemin d'accès au fichier contenant la grille avec les coordonnées des sommets des sources
+ps_file_path = 'ps_verif.npy' #le chemin d'accès au fichier qui contient la grille de presisons  
+rs_file_path = 'rs_verif.npy' #le chemin d'accès au fichier contenant la grille avec les coordonnées des sommets des sources
 E = 75e9
 v = 0.25
 
