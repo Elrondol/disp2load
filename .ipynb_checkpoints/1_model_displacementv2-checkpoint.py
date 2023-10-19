82328dc0b9743a7d033ff0a5690ab3f61791c99b
Nicolas Paris nicolas.paris3@etu.univ-grenoble-alpes.fr <nicolas.paris3@etu.univ-grenoble-alpes.fr>
2023-10-19 21:09:53 +0200

    t

--- .ipynb_checkpoints/1_model_displacementv2-checkpoint.py
+++ .ipynb_checkpoints/1_model_displacementv2-checkpoint.py
@@ -8,19 +8,19 @@ import tempfile
 ce script a une parallélisaiton naturelle : il créé un fiichier pour indiquer qu'il travail sur une ligne donnée  et il créé le fichier de la ligne et supprime le fichier lock -> on peut lancer le job autant de fois que l'on veut et chacun se mettra à travailler sur une ligne diféfrente  """
 
 #################### OU VA T ON EXECUTER LE RUN #### -> les fichiers avec les lignes de sources seront rassemblés dans ce dossier 
-directory = 'run_verif_convert' 
+directory = 'run_verif2' 
 
 ######################################## SOURCES ET CONSTANTES ################
 
-ps_file_path = 'ps_verif_convert.npy' #le chemin d'accès au fichier qui contient la grille de presisons  
-rs_file_path = 'rs_verif_convert.npy' #le chemin d'accès au fichier contenant la grille avec les coordonnées des sommets des sources
+ps_file_path = 'ps_verif2.npy' #le chemin d'accès au fichier qui contient la grille de presisons  
+rs_file_path = 'rs_verif2.npy' #le chemin d'accès au fichier contenant la grille avec les coordonnées des sommets des sources
 E = 75e9
 v = 0.25
 
 ###################################### POSITIONS À CALCULER ###########
 
-xs = np.linspace(-1932000,-1735000, 200)  #nested loops -> il calcule la grille complète
-ys = np.linspace(-4406000,-4442000, 200)
+xs = np.linspace(288000,459000, 200)  #nested loops -> il calcule la grille complète
+ys = np.linspace(4486000,4650000, 200)
 zs = np.linspace(0,2,3)
 
 #################################################################################################################################
@@ -37,8 +37,10 @@ except:
 ps = np.load(ps_file_path)  
 rs = np.load(rs_file_path)
 
-for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the displacement for the following line of 
+vy_idx = 0
 
+while vy_idx <  len(ps[:,0]):
+# for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the displacement for the following line of 
     #### on vérifie si y'a pas déjà un job en train de bosser sur la ligne 
     if  os.path.exists(f'{directory}/source_line_{vy_idx:03}.lock')==False and os.path.exists(f'{directory}/source_line_{vy_idx:03}.npy')==False: #si pas en train de calculer et pas déjà calculée
         #### doit maintenant créer le fichier temporaire pour lock la ligne
@@ -50,9 +52,7 @@ for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the 
         for vx_idx in range(len(ps[0,:])):
             p = ps[vy_idx,vx_idx]
             r = rs[vy_idx,vx_idx]
-            if p ==0: #If load is 0, then we don't need to perform the computation
-                Us[:,:,:,:] = 0
-            else:
+            if p !=0: #If load is 0, then we don't need to perform the computation so we put the condition stating that it must be different than 0
                 for i, x in enumerate(xs):
                     for j, y in enumerate(ys):
                         for k, z in enumerate(zs):
@@ -62,3 +62,5 @@ for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the 
         np.save(f'{directory}/source_line_{vy_idx:03}.npy', Us) #saving the computed line 
         ## il  a créé le ficihier de la ligne; on peut par conséquent supprimer le lock file 
         os.remove(f'{directory}/source_line_{vy_idx:03}.lock')
+    
+    vy_idx += 1 
--- 1_model_displacementv2.py
+++ 1_model_displacementv2.py
@@ -1,19 +1,18 @@
 import numpy as np
 import load2disp
 import os
-import tempfile
 
 """#ici on va devoir dire quelles lignes de source il va calculer avec ce run, il dva calculer ça pour tous les points renseigner 
 
 ce script a une parallélisaiton naturelle : il créé un fiichier pour indiquer qu'il travail sur une ligne donnée  et il créé le fichier de la ligne et supprime le fichier lock -> on peut lancer le job autant de fois que l'on veut et chacun se mettra à travailler sur une ligne diféfrente  """
 
 #################### OU VA T ON EXECUTER LE RUN #### -> les fichiers avec les lignes de sources seront rassemblés dans ce dossier 
-directory = 'run_verif' 
+directory = 'run_verif2' 
 
 ######################################## SOURCES ET CONSTANTES ################
 
-ps_file_path = 'ps_verif.npy' #le chemin d'accès au fichier qui contient la grille de presisons  
-rs_file_path = 'rs_verif.npy' #le chemin d'accès au fichier contenant la grille avec les coordonnées des sommets des sources
+ps_file_path = 'ps_verif2.npy' #le chemin d'accès au fichier qui contient la grille de presisons  
+rs_file_path = 'rs_verif2.npy' #le chemin d'accès au fichier contenant la grille avec les coordonnées des sommets des sources
 E = 75e9
 v = 0.25
 
@@ -37,8 +36,10 @@ except:
 ps = np.load(ps_file_path)  
 rs = np.load(rs_file_path)
 
-for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the displacement for the following line of 
+vy_idx = 0
 
+while vy_idx <  len(ps[:,0]):
+# for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the displacement for the following line of 
     #### on vérifie si y'a pas déjà un job en train de bosser sur la ligne 
     if  os.path.exists(f'{directory}/source_line_{vy_idx:03}.lock')==False and os.path.exists(f'{directory}/source_line_{vy_idx:03}.npy')==False: #si pas en train de calculer et pas déjà calculée
         #### doit maintenant créer le fichier temporaire pour lock la ligne
@@ -60,3 +61,5 @@ for vy_idx in range(len(ps[:,0])):  #we loop over rows so that we can clear the 
         np.save(f'{directory}/source_line_{vy_idx:03}.npy', Us) #saving the computed line 
         ## il  a créé le ficihier de la ligne; on peut par conséquent supprimer le lock file 
         os.remove(f'{directory}/source_line_{vy_idx:03}.lock')
+    
+    vy_idx += 1 
